c854a15dccb28f5b9db45e13399d869514865d82
lrivales <87040442+lrivales@users.noreply.github.com>
2021-11-02 20:24:32 -0400

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'l@wr3nc30871',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
